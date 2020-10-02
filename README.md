1976330d6737b85447d821e93f1aa1f0417f4370
A.J. Orians <ajorians@gmail.com>
2020-10-02 18:10:21 -0400

    Added initial files.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # WPFMenuItemHeaderDemo
+
+Just a small program to show how to format text in a WPF Menu Item's header:
+
+![image](https://user-images.githubusercontent.com/3475163/94973721-680d0f00-04da-11eb-8f67-9f9a0cffbffd.png)
+
+:smiley:
---
+++ Test_MenuItem/ProductVersion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_MenuItem
+{
+   public class ProductVersion
+   {
+      public const string ProductName = "Some text";
+   }
+}
